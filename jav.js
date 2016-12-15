cee17057f30dde9f067bd343d8cd1d1766f85028
Khaled ouziala <khaledouziala@Khaleds-MacBook-Pro-2.local>
2016-12-14 20:21:33 -0600

    initial files

---
+++ jav.js
@@ -0,0 +1,30 @@
+	// Ajax
+			$(document).ready(function(){
+		    $.ajax({
+		        url: "https://data.policefoundation.org/resource/iibt-hvrs.json",
+		        type: "GET",
+		        data: {
+		          "$limit" : 5000,
+		          "$$app_token" : "nx6iorDFMna26v0zqNiSfz0ZK"
+		        }
+		    }).done(function(data){
+		      alert("Retrieved " + data.length + " records from the dataset!");
+		      console.log(data);
+		    });
+		});
+
+
+	var map;	// Google map object (global variable)
+	
+	// Initialize and display a google map
+	function Init()
+	{
+		// Create a Google coordinate object for where to center the map
+		var latlngDC = new google.maps.LatLng( 30.2672, -97.7431 );	// Coordinates of Washington, DC (area centroid)
+		
+		// Map options for how to display the Google map
+		var mapOptions = { zoom: 12, center: latlngDC  };
+		
+		// Show the Google map in the div with the attribute id 'map-canvas'.
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	}
