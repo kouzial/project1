29001c0971c9999c56e00b826d2a97703eda545f
Jeremy Harmon <jharmonious@Jeremys-MacBook-Air.local>
2016-12-17 13:22:01 -0600

    geolocator added

--- jav.js
+++ jav.js
@@ -1,10 +1,11 @@
 // Ajax
+
 $(document).ready(function() {
     $.ajax({
         url: "https://data.policefoundation.org/resource/iibt-hvrs.json",
         type: "GET",
         data: {
-            "$limit": 500,
+            "$limit": 5,
             "$$app_token": "nx6iorDFMna26v0zqNiSfz0ZK"
         }
     }).done(function(data) {
@@ -21,7 +22,8 @@ $(document).ready(function() {
                     "$limit": 5,
                 }
             }).done(function(data) {
-                console.log(data);
+                var gresults = data;
+								console.log(gresults);
             });
 
         }
@@ -45,3 +47,59 @@ function Init() {
     // Show the Google map in the div with the attribute id 'map-canvas'.
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 }
+
+
+
+	function writeAddressName(latLng) {
+		var geocoder = new google.maps.Geocoder();
+		geocoder.geocode({
+			"location": latLng
+		},
+		function(results, status) {
+			if (status == google.maps.GeocoderStatus.OK)
+				document.getElementById("address").innerHTML = results[0].formatted_address;
+			else
+				document.getElementById("error").innerHTML += "Unable to retrieve your address" + "<br />";
+		});
+	}
+
+	function geolocationSuccess(position) {
+		var userLatLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+		// Write the formatted address
+		writeAddressName(userLatLng);
+
+		var myOptions = {
+			zoom : 16,
+			center : userLatLng,
+			mapTypeId : google.maps.MapTypeId.ROADMAP
+		};
+		// Draw the map
+		var mapObject = new google.maps.Map(document.getElementById("map-canvas"), myOptions);
+		// Place the marker
+		new google.maps.Marker({
+			map: mapObject,
+			position: userLatLng
+		});
+	}
+
+	function geolocationError(positionError) {
+		document.getElementById("error").innerHTML += "Error: " + positionError.message + "<br />";
+	}
+
+	function geolocateUser() {
+		// If the browser supports the Geolocation API
+		if (navigator.geolocation)
+		{
+			var positionOptions = {
+				enableHighAccuracy: true,
+				timeout: 10 * 1000 // 10 seconds
+			};
+			navigator.geolocation.getCurrentPosition(geolocationSuccess, geolocationError, positionOptions);
+		}
+		else
+			document.getElementById("error").innerHTML += "Your browser doesn't support the Geolocation API";
+	}
+
+	$("#locatorIcon").on("click", function(){
+		geolocateUser();
+	});
